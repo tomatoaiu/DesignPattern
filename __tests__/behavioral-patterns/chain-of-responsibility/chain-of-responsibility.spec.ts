3c769e758917e41e836ff4118ed42a786d68acf4
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-11-21 16:32:56 +0900

    feat: chain of responsibility pattern

---
+++ __tests__/behavioral-patterns/chain-of-responsibility/chain-of-responsibility.spec.ts
@@ -0,0 +1,15 @@
+import { Japanese, English, LanguageHandler } from '../../../src/behavioral-patterns/chain-of-responsibility/chain-of-responsibility'
+
+describe('Mediator Pattern', () => {
+  const languageHandler: LanguageHandler = new LanguageHandler();
+  languageHandler.setLanguage(new Japanese());
+  languageHandler.setLanguage(new English());
+
+  it('こんにちは is Japanese', () => {
+    expect(languageHandler.whatLanguge('こんにちは')).toBeInstanceOf(Japanese)
+  })
+
+  it('Hello is English', () => {
+    expect(languageHandler.whatLanguge('Hello')).toBeInstanceOf(English)
+  })
+})
