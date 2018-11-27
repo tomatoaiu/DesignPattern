d6575ad48ec743fa929d41819958c6e0a6848599
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-11-27 16:58:51 +0900

    feat: decorator pattern

--- src/structural-patterns/decorator-pattern/decorator.ts
+++ src/structural-patterns/decorator-pattern/decorator.ts
@@ -1,1 +1,23 @@
-class
+export class Rice {
+  topping: string;
+
+  constructor () {
+    this.topping = 'rice';
+  }
+}
+
+export class SalmonRice extends Rice {
+  constructor (rice: Rice) {
+    super();
+    this.topping = `salmon ${rice.topping}`;
+  }
+}
+
+export class Ochazuke extends Rice {
+  rice: Rice;
+
+  constructor (rice: Rice) {
+    super();
+    this.topping = `tea ${rice.topping}`;
+  }
+}
